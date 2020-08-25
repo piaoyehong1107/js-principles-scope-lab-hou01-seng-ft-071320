1bcbb922d391dfa91f8c1068cb5f9b483fa1c85e
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-25 18:52:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution in this file!
 var customerName='bob'
 function upperCaseCustomerName(name){
-  return customerName.toUpperCase();
+  return name.toUpperCase();
 }
 
 function setBestCustomer(){
