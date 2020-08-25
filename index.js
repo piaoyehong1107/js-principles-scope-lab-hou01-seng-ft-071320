f9492054f61c9bbe49194fdc317038f621dbe49c
Yehong Piao <piaoyehong1107@gmail.com>
2020-08-25 18:51:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerNam='bob'
+function upperCaseCustomerName(){
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+  return bestCustomer='not bob';
+}
+
+function overwriteBestCustomer(){
+  return bestCustomer='maybe bob';
+}
+
+const leastFavoriteCustomer = 'tom';
+function changeLeastFavoriteCustomer(){
+  return leastFavoriteCustomer = 'jimmy';
+}
